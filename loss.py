4fa264cc82fc72a56b9c57e409b54966293eb8a6
Andrzej Pisarek <andrzej.r.pisarek@gmail.com>
2019-05-15 22:32:59 +0300

    Implement perceptual loss

---
+++ loss.py
@@ -0,0 +1,48 @@
+import torch
+from torch import nn as nn
+from torchvision.models.vgg import vgg19
+
+
+device = "cpu"
+cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        # normalize img
+        return (img - self.mean) / self.std
+
+
+class PerceptualNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        vgg = vgg19(pretrained=True)
+        features_slice = vgg.features[:35]
+        perceptual_network = nn.Sequential(*features_slice).eval()
+        for param in perceptual_network.parameters():
+            param.requires_grad = False
+        self.perceptual_network = perceptual_network
+
+    def forward(self, inputs):
+        return self.perceptual_network(inputs)
+
+
+class PerceptualLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.perceptual_network = PerceptualNetwork()
+        self.mse_loss = nn.MSELoss()
+
+    def forward(self, input_a, input_b):
+        return self.mse_loss(
+            self.perceptual_network(input_a), self.perceptual_network(input_b)
+        )
--- main.py
+++ main.py
@@ -2,6 +2,26 @@ import torch
 
 from networks.generator import Generator
 from networks.discriminator import Discriminator
+from loss import PerceptualLoss
+import torch.nn as nn
+
+
+# create a module to normalize input image so we can easily put it in a
+# nn.Sequential
+class FeatureExtractor(nn.Module):
+    def __init__(self, submodule, extracted_layers):
+        super().__init__()
+        self.extracted_layers = extracted_layers
+        self.submodule: nn.Module = submodule
+
+    def forward(self, x):
+        outputs = []
+        for name, module in self.submodule.named_modules():
+            x = module(x)
+            if name in self.extracted_layers:
+                outputs += [x]
+        return outputs + [x]
+
 
 if __name__ == "__main__":
     gen_net = Generator()
@@ -13,3 +33,7 @@ if __name__ == "__main__":
     dis_net = Discriminator()
     print(dis_net)
     print(dis_net(output).shape)
+    perceptual_loss = PerceptualLoss()
+
+    import ipdb; ipdb.set_trace()
+
